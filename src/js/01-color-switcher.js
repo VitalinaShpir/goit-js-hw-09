25d105140bf4800cbaf890667cb30daf39db400c
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-17 18:09:18 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
+
+
+startButton.addEventListener("click", startColorChange);
+stopButton.addEventListener("click", stopColorChange);
+
+
+let intervalId;
+
+function startColorChange() {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopColorChange() {
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+  let randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+// console.log(startButton);
+// console.log(stopButton);
