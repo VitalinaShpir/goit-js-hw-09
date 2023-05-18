6bd6161b5ae85c34fd6cd26109031328af728b3b
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-19 00:03:41 +0300

    task2
    not ready

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,11 +2,15 @@ import flatpickr from "flatpickr";
 
 import "flatpickr/dist/flatpickr.min.css";
 
-const dateTimePicker = document.querySelector('input#datetime-picker');
+const dateTimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('button[data-start]');
 // console.log(dateTimePicker);
+// startButton.addEventListener('click', countdownTimer);
+
 const options = {
     enableTime: true,
     time_24hr: true,
+    minDate: "today",
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
@@ -14,4 +18,21 @@ const options = {
     },
   };
 
-flatpickr(dateTimePicker, options)
+flatpickr('#datetime-picker', options)
+
+//   const countdownTimer = setInterval(function() {
+//     timeDiff -= 1000; 
+//     const seconds = Math.floor((timeDiff / 1000) % 60); 
+//     const minutes = Math.floor((timeDiff / 1000 / 60) % 60); 
+//     const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+//     const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+//     const timerDisplay = document.getElementById("timer"); 
+//     timerDisplay.textContent = days + "д " + hours + "г " + minutes + "хв " + seconds + "с"; 
+//     if (timeDiff <= 0) {
+//       clearInterval(countdownTimer); 
+//       timerDisplay.textContent = "Час вичерпано!";
+//     }
+//   }, 1000); 
+
+
+console.log('hello');
