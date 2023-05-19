08373c25b576df248e7a1228e44ac0f924615781
VitalinaShpir <grigorenko.vitalina@gmail.com>
2023-05-19 16:49:59 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,38 +1,62 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
-const dateTimePicker = document.querySelector('#datetime-picker');
+const inputTimePicker = document.querySelector('#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
-// console.log(dateTimePicker);
-// startButton.addEventListener('click', countdownTimer);
+
+const secTime = document.querySelector('span[data-seconds]');
+const minTime = document.querySelector('span[data-minutes]');
+const hourTime = document.querySelector('span[data-hours]');
+const dayTime = document.querySelector('span[data-days]');
+// console.log(inputTimePicker);
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    minDate: "today",
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+  
+  const timer = setInterval(() => {
+       
+        const selectedDate = selectedDates[0];
+        const currentDate = new Date();
 
-flatpickr('#datetime-picker', options)
+        if (selectedDate > currentDate) {
+        const ms = selectedDate.getTime() - currentDate.getTime();
+        const convertedMs = convertMs(ms);
 
-//   const countdownTimer = setInterval(function() {
-//     timeDiff -= 1000; 
-//     const seconds = Math.floor((timeDiff / 1000) % 60); 
-//     const minutes = Math.floor((timeDiff / 1000 / 60) % 60); 
-//     const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
-//     const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-//     const timerDisplay = document.getElementById("timer"); 
-//     timerDisplay.textContent = days + "д " + hours + "г " + minutes + "хв " + seconds + "с"; 
-//     if (timeDiff <= 0) {
-//       clearInterval(countdownTimer); 
-//       timerDisplay.textContent = "Час вичерпано!";
-//     }
-//   }, 1000); 
+        secTime.textContent = convertedMs.seconds;
+        minTime.textContent = convertedMs.minutes;
+        hourTime.textContent = convertedMs.hours;
+        dayTime.textContent = convertedMs.days;}
+
+        else{
+          clearInterval(timer);
+          return alert('Please choose a date in the future');
+        }
+      }, 1000);
+
+  },
+};
 
 
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const dateTimePicker = flatpickr('#datetime-picker', options);
+
+startButton.addEventListener('click', dateTimePicker);
 console.log('hello');
